d4c27e412ceb2dcefa8c112607c86ee40c9c5d6d
Colorathe <joacovulcano@gmail.com>
2022-10-21 18:02:45 -0300

    se añadio botones funcionales

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,3 +1,4 @@
+import Button from "./Button";
 import CalculatorScreen from "./CalculatorScreen";
 import CalculatorState from "./CalculatorState";
 
@@ -7,7 +8,28 @@ const Calculator = () => {
       <CalculatorState>
         <div className='calculatorContainer'>
           <CalculatorScreen />
-          <div className='container'></div>
+          <div className='container'>
+            <Button type='action' value='AC' />
+            <Button type='operator' value='%' />
+            <Button type='action' value='<==' />
+            <Button type='operator' value='/' />
+            <Button type='number' value='7' />
+            <Button type='number' value='8' />
+            <Button type='number' value='9' />
+            <Button type='operator' value='*' />
+            <Button type='number' value='4' />
+            <Button type='number' value='5' />
+            <Button type='number' value='6' />
+            <Button type='operator' value='-' />
+            <Button type='number' value='1' />
+            <Button type='number' value='2' />
+            <Button type='number' value='3' />
+            <Button type='operator' value='+' />
+            <Button type='action' value='+/-' />
+            <Button type='number' value='0' />
+            <Button type='action' value='.' />
+            <Button type='action' value='=' />
+          </div>
         </div>
       </CalculatorState>
     </>
